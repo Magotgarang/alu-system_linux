cbc7369727726a62231b1bf45163fdc26ebdefde
Magotgarang <m.guut@alustudent.com>
2024-11-08 04:54:28 -0800

    updated

--- multithreading/11-blur_image.c
+++ multithreading/11-blur_image.c
@@ -1,128 +1,77 @@
-#include "multithreading.h"
-#include <stdio.h>
-#include <errno.h>
-#include <signal.h>
+#include <pthread.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include "multithreading.h"
 
-#define THREAD_POOL_SZ 10
+#define MAX_THREADS 16
 
+// Structure for thread arguments
+typedef struct {
+    img_t *img_blur;
+    img_t const *img;
+    kernel_t const *kernel;
+    size_t start;
+    size_t end;
+} thread_arg_t;
 
-/**
- * blurPortionThreadEntry - thread entry function for applyinh a Gaussian Blur
- *   to a portion of a .ppm image
- *
- * @portion: pointer to profile of portion of .ppm image to be blurred
- * Return: always NULL, `void *` expected by pthread_create param prototype
- */
-void *blurPortionThreadEntry(blur_portion_t *portion)
-{
-	if (!portion)
-		pthread_exit(NULL);
+// Function to apply kernel on a specific image portion (called by each thread)
+void *blur_portion(void *arg) {
+    thread_arg_t *targ = (thread_arg_t *)arg;
+    size_t x, y, i, j;
+    int half_k = targ->kernel->size / 2;
 
-	blur_portion(portion);
+    for (y = targ->start; y < targ->end; y++) {
+        for (x = 0; x < targ->img->w; x++) {
+            float r = 0, g = 0, b = 0;
+            float kernel_sum = 0;
 
-	pthread_exit(NULL);
+            // Convolution operation
+            for (i = 0; i < targ->kernel->size; i++) {
+                for (j = 0; j < targ->kernel->size; j++) {
+                    int pixel_x = x + i - half_k;
+                    int pixel_y = y + j - half_k;
+
+                    // Check bounds
+                    if (pixel_x >= 0 && pixel_x < targ->img->w && pixel_y >= 0 && pixel_y < targ->img->h) {
+                        pixel_t *pixel = &targ->img->pixels[pixel_y * targ->img->w + pixel_x];
+                        float kernel_val = targ->kernel->matrix[i][j];
+
+                        r += pixel->r * kernel_val;
+                        g += pixel->g * kernel_val;
+                        b += pixel->b * kernel_val;
+                        kernel_sum += kernel_val;
+                    }
+                }
+            }
+
+            // Normalize the values
+            size_t idx = y * targ->img->w + x;
+            targ->img_blur->pixels[idx].r = (unsigned char)(r / kernel_sum);
+            targ->img_blur->pixels[idx].g = (unsigned char)(g / kernel_sum);
+            targ->img_blur->pixels[idx].b = (unsigned char)(b / kernel_sum);
+        }
+    }
+    return NULL;
 }
 
+// Main function for blurring the image using multiple threads
+void blur_image(img_t *img_blur, img_t const *img, kernel_t const *kernel) {
+    pthread_t threads[MAX_THREADS];
+    thread_arg_t args[MAX_THREADS];
+    size_t thread_count = MAX_THREADS;
+    size_t rows_per_thread = img->h / thread_count;
 
-/**
- * portionImage - divides pixel coordinate field of a .ppm image into
- *   horizontal strips and stores portion profiles in array of blur_portion_t
- *   structs
- *
- * @img_blur: pointer to blurred image
- * @img: pointer to source image
- * @kernel: pointer to the convolution kernel to use for Gaussian blur
- * @portion_ct: number of portions to divide pixel coordinates into
- * Return: array of portion profiles, or NULL on failure
- */
-blur_portion_t *portionImage(img_t *img_blur, const img_t *img,
-			     const kernel_t *kernel, size_t portion_ct)
-{
-	blur_portion_t *portions = NULL;
-	size_t i, extra_rows, next_y;
+    for (size_t i = 0; i < thread_count; i++) {
+        args[i].img_blur = img_blur;
+        args[i].img = img;
+        args[i].kernel = kernel;
+        args[i].start = i * rows_per_thread;
+        args[i].end = (i == thread_count - 1) ? img->h : (i + 1) * rows_per_thread;
 
-	if (!img_blur || !img || !kernel ||
-	    !img_blur->pixels || !img->pixels || !kernel->matrix)
-		return (NULL);
+        pthread_create(&threads[i], NULL, blur_portion, &args[i]);
+    }
 
-	portions = malloc(sizeof(blur_portion_t) * portion_ct);
-	if (!portions)
-		return (NULL);
-
-	/* assign portions by dividing image into horizontal strips */
-	for (i = 0, next_y = 0, extra_rows = img->h % portion_ct;
-	     i < portion_ct; i++)
-	{
-		portions[i].img = img;
-		portions[i].img_blur = img_blur;
-
-		portions[i].x = 0;
-		portions[i].y = next_y;
-		portions[i].w = img->w;
-		portions[i].h = img->h / portion_ct;
-		if (extra_rows)
-		{
-			(portions[i].h)++;
-			extra_rows--;
-		}
-
-		portions[i].kernel = kernel;
-
-		next_y += portions[i].h;
-	}
-
-	return (portions);
-}
-
-
-/**
- * blur_image - blurs the entirety of an image using a Gaussian Blur
- *
- * @img_blur: pointer to blurred image
- * @img: pointer to source image
- * @kernel: pointer to the convolution kernel to use
- */
-void blur_image(img_t *img_blur, img_t const *img, kernel_t const *kernel)
-{
-	pthread_t t_ids[THREAD_POOL_SZ] = {0};
-	blur_portion_t *portions = NULL;
-	pthread_attr_t attr;
-	size_t i, j;
-
-	if (!img_blur || !img || !kernel ||
-	    !img_blur->pixels || !img->pixels || !kernel->matrix)
-		return;
-	pthread_attr_init(&attr); /* explicit attribute for portabiliity */
-	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-	portions = portionImage(img_blur, img, kernel, THREAD_POOL_SZ);
-	if (!portions)
-	{
-		fprintf(stderr, "blur_image: portionImage failed\n");
-		return;
-	}
-	for (i = 0; i < THREAD_POOL_SZ; i++)
-	{
-		if (pthread_create(t_ids + i, &attr,
-				   (void *(*)(void *))blurPortionThreadEntry,
-				   portions + i) != 0)
-		{
-			fprintf(stderr, "blur_image: pthread_create failed\n");
-			for (j = 0; j < i; j++)
-				pthread_kill(t_ids[j], SIGTERM);
-			free(portions);
-			return;
-		}
-	}
-	pthread_attr_destroy(&attr);
-	for (i = 0; i < THREAD_POOL_SZ; i++)
-	{
-		if (pthread_join(t_ids[i], NULL) != 0)
-		{
-			fprintf(stderr, "blur_image: pthread_join failed\n");
-			free(portions);
-			return;
-		}
-	}
-	free(portions);
+    for (size_t i = 0; i < thread_count; i++) {
+        pthread_join(threads[i], NULL);
+    }
 }
